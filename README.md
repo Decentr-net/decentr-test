1aae256297d16f87a676d8e7e632a755c0b87bde
Andrei Shneider <andrei.shneider@scorum.com>
2020-12-08 17:31:44 +0300

    README added

---
+++ README.md
@@ -0,0 +1,8 @@
+## Decentr Test
+
+This assumes that you're running Linux or MacOS and have installed [Go 1.15+](https://golang.org/dl/).  
+This guide helps you:
+
+1. Clone decentr `git clone git@github.com:Decentr-net/decentr.git`
+2. Go to the `decentr` folder
+3. Run `make install`. It builds both `decentrd` and `decentrcli`. 
--- test.js
+++ test.js
@@ -13,29 +13,35 @@ describe('community', function() {
         this.timeout(10000)
         shell.config.silent = true;
 
+        // remove decentr home folders
         shell.rm('-rf', '~/.decentrd')
         shell.rm('-rf',  '~/.decentrcli')
 
         shell.exec('decentrd init test --chain-id=' + chainId)
 
+        // configure decentrcli
         shell.exec('decentrcli config output json')
         shell.exec('decentrcli config trust-node true')
         shell.exec('decentrcli config chain-id testnet')
         shell.exec('decentrcli config keyring-backend test')
 
+        // save two accounts with keys
         // yes, output is in stderr
         jack = JSON.parse(shell.exec('decentrcli keys add jack').stderr)
         alice = JSON.parse(shell.exec('decentrcli keys add alice').stderr)
 
+        // prepare genesis.json
         shell.exec('decentrd add-genesis-account $(decentrcli keys show jack -a) 1000000udec')
         shell.exec('decentrd add-genesis-account $(decentrcli keys show alice -a) 1000000udec')
-
         shell.exec('decentrd gentx --name jack --keyring-backend test --amount 1000000udec')
         shell.exec('decentrd collect-gentxs')
         shell.exec('decentrd validate-genesis')
+
+        // run the node
         decentrd = shell.exec('decentrd start', {async: true})
         decentrd.stdout.on('data', function (data) {
             if (data.includes("Executed block")) {
+                // run the rest server connected to the node
                 decentcli = shell.exec('decentrcli rest-server', {async: true})
                 done()
             }
